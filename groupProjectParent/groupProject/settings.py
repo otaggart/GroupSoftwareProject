8c107a97302138fe11f1074e0f6434c71ac44d07
aSaunEXCS <161340654+aSaunEXCS@users.noreply.github.com>
2025-03-25 08:19:48 +0000

    Update settings.py

--- groupProjectParent/groupProject/settings.py
+++ groupProjectParent/groupProject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import mimetypes
+mimetypes.add_type("image/png", ".png", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
