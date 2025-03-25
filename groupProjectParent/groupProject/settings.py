fe5158a5e3fa2bb6cdfc87593d951112c420ed76
OscarTaggart <oet202@exeter.ac.uk>
2025-03-25 07:40:53 +0000

    update

--- groupProjectParent/groupProject/settings.py
+++ groupProjectParent/groupProject/settings.py
@@ -130,8 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR / "static"),
 ]
 
 # Media files (Uploaded files, e.g. profile images)
